890a06b25f7476698841bd9ae9599a44709292d1
welch <devnull@localhost>
2003-09-11 03:34:52 +0000

    Added -d and -v to man page. Added BSD license.

--- grid-proxy/grid-proxy.sh
+++ grid-proxy/grid-proxy.sh
@@ -338,9 +338,13 @@ Commandline options are:
 
 =item -c Csh mode (default)
 
+=item -d Run grid-proxy-init with '-debug'
+
 =item -f Generate new proxy even if one exists.
 
 =item -h Print usage and exit
+
+=item -v Run grid-proxy-init with '-verify'
 
 =back
 
@@ -348,13 +352,43 @@ Commandline options are:
 
 Von Welch welch@mcs.anl.gov
 
-This is free software. You may do with it what you please.  No support
-or warrantly given or implied. Bug fixes or comments welcome.
-
 =head1 ACKNOWLEDGMENTS
 
 Tom Goodale (goodale@aei-potsdam.mpg.de) for a bunch of bug fixes and
 bash directions.
+
+=head1 LICENSE
+
+Copyright (c) 2003, Von Welch
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+* Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+* Neither the name of Von Welch nor the names of its contributors may
+be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =cut
 
