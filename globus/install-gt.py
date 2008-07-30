0f311d8258e6da4c9e61e8d05cd1af327176bc9d
vwelch <devnull@localhost>
2008-07-30 14:36:48 +0000

    Added OutputHandler class.

--- globus/install-gt.py
+++ globus/install-gt.py
@@ -9,15 +9,12 @@
 #
 ######################################################################
 
-defaultGlobusVersion="4.0.7"
-
-######################################################################
-
 import datetime
 import optparse
 import os
 import os.path
 import subprocess
+import sys
 import tempfile
 
 ######################################################################
@@ -30,63 +27,65 @@ confOptions = [
 
 ######################################################################
 #
+# Globals
+
+# Handle to which we push normal output
+outputHandle = None
+
+######################################################################
+#
 # Utility functions
 
 def errorExit(msg, exitValue=1):
     """Print error message and exit with exitValue."""
-
-    stderr.write(msg)
+            
+    sys.stderr.write(msg.rstrip('\n') + "\n")
     sys.exit(exitValue)
 
 ######################################################################
 
-def main():
-    # Parse our arguments
-    usage = "usage: %prog [options] <Install Path>"
-    parser = optparse.OptionParser(usage)ß
-    parser.add_option("-v", "--globus-version", dest="globusVersion",
-                      default=defaultGlobusVersion,
-                      help="Specify version of Globus Toolkit to install (default is %default)")
-    (options, args) = parser.parse_args()
-    if len(args) != 1:
-        parser.error("Missing path for installation")
-    globusLocation = args.pop(0)
+class OutputHandler:
+    """Handle output for process."""
 
-    # Check for required environment variables
-    for var in ["JAVA_HOME", "ANT_HOME"]:
-        if not os.environ.has_key(var):
-            errorExit("Environment variable %s is not set" % var)
+    def __init__(self, logFilename, appendLog = True):
+        # Open our logfile and create a tee process to it
+        teeCmdArgs = ["tee"]
+        if appendLog:
+            teeCmdArgs.append("-a")
+        teeCmdArgs.append(logFilename)
+        self.pipe = subprocess.Popen(teeCmdArgs, stdin = subprocess.PIPE)
+        self.handle = self.pipe.stdin
 
-    # Make sure install paths exists and set GLOBUS_LOCATION
-    if not os.path.exists(globusLocation):
-        os.makedirs(globusLocation)
-    os.environ['GLOBUS_LOCATION'] = globusLocation
+    def message(self, msg):
+        """Handle outputing a message."""
 
-    # Open our logfile and create a tee process to it
-    logFilename = os.path.join(globusLocation, "install-gt.log")
-    teePipe = subprocess.Popen(["tee", "-a", logFilename],
-                               stdin = subprocess.PIPE)
-    toTee = teePipe.stdin
-    
-    # Create functions for easy logging
-    logMessage = lambda msg: toTee.write(msg.rstrip('\n') + "\n")
-    logCmd = lambda args: subprocess.Popen(args,
-                                           stdout=toTee,
-                                           stderr=toTee).returncode
+        self.handle.write(msg.strip('\n') + "\n")
 
-    def flaggedCmd(flagFile, args, description=None, exitOnError=True):
+    def logCmd(self, args):
+        """Run command in args, logging output and returning returncode."""
+        self.message("Executing command: " + " ".join(args))
+        pipe = subprocess.Popen(args,
+                                stdout=self.handle,
+                                stderr=self.handle)
+        status = pipe.wait()
+        return status
+
+    def flaggedCmd(self, flagFile, args,
+                   description=None, exitOnError=True):
         """If flagFile does not exist, print description and execute args.
-
+            
         If exitOnError is True, exits if command fails. Otherwise returns
         status. If flagFile exists, always returns zero."""
         if os.path.exists(flagFile):
             return 0
         if description:
-            logMessage(description)
-        status = logCmd(args)
+            self.message(description)
+        status = self.logCmd(args)
+        if status is None:
+            errorExit("Command returned status of None.")
         if status != 0:
             if exitOnError:
-                errorExit("Command returned non-zero status.")
+                errorExit("Command returned non-zero status (%d)." % status)
             else:
                 return status
         # Touch the file, if the cmd didn't create it
@@ -94,34 +93,112 @@ def main():
             open(flagFile, 'w').close()
         return status
 
+    def cleanup(self):
+        self.handle.flush()
+        self.handle.close()
+        self.pipe.wait()
+
+######################################################################
+
+def main():
+    # Parse our arguments
+    usage = "usage: %prog [options] <GLOBUS_LOCATION>"
+    parser = optparse.OptionParser(usage)
+    parser.add_option("-a", "--appendLog",
+                      action="store_true",
+                      dest="appendLog",
+                      help="Append to log file (instead of overwriting it) (default is %default)")
+    parser.add_option("-C", "--clearPrevious",
+                      action="store_true",
+                      dest="clearPrevious",
+                      help="Clear any previous build and install (defalt is %default)")
+    parser.add_option("-l", "--logFilePath",
+                      dest="logFilePath",
+                      metavar="PATH",
+                      help="Specify PATH for logfile (default is %default)")
+    parser.add_option("-s", "--sourceCachePath",
+                      dest="sourceCachePath",
+                      metavar="PATH",
+                      help="Specify PATH to cache GT tarballs (default is %default)")
+    parser.add_option("-v", "--globus-version", dest="globusVersion",
+                      metavar="VERSION",
+                      help="Specify VERSION of Globus Toolkit to install (default is %default)")
+    parser.set_defaults(appendLog = False,
+                        clearPrevious = False,
+                        globusVersion = "4.0.7",
+                        logFilePath = "$GLOBUS_LOCATION/gt-install.log",
+                        sourceCachePath = ".")
+    (options, args) = parser.parse_args()
+
+    # Make sure install paths exists and set GLOBUS_LOCATION
+    if len(args) != 1:
+        parser.error("Missing path for installation")
+    globusLocation = os.path.abspath(args.pop(0))
+    if not os.path.exists(globusLocation):
+        os.makedirs(globusLocation)
+    os.environ['GLOBUS_LOCATION'] = globusLocation
+
+    # Now that GLOBUS_LOCATION is set, parse other values which
+    # may depend on it
+    globusVersion = options.globusVersion
+    sourceCachePath = os.path.abspath(options.sourceCachePath)
+    appendLog = options.appendLog
+    clearPrevious = options.clearPrevious
+    logFilePath = os.path.expandvars(options.logFilePath)
+
+    # Check for required environment variables
+    for var in ["JAVA_HOME", "ANT_HOME"]:
+        if not os.environ.has_key(var):
+            errorExit("Environment variable %s is not set" % var)
+
     # Create a temporary directory for working
-    tmpDir = tempfile.mkdtemp()
+    tmpDir = os.path.join(tempfile.gettempdir(),
+                          "install-gt-%d" % os.getuid())
+    if not os.path.exists(tmpDir):
+        os.makedirs(tmpDir, 0700)
 
     globusSourceDir="gt%s-all-source-installer" % globusVersion
     globusTarball="%s.tar.gz" % globusSourceDir
 
     # Full path to our unpacked source
-    gtSourcePath = os.path.join(tmpDir, globusSourceDir)
+    globusSourcePath = os.path.join(tmpDir, globusSourceDir)
     
     # Calculate our flag files
     gtInstalledFlag = os.path.join(globusLocation, "gt-installed")
-    gtUnpackedFlag=os.path.join(globusSourcePath, "gt-unpacked")
-    gtConfiguredFlag=os.path.join(globusSourcePath, "gt-configured")
-    gtMadeFlag=os.path.join(globusSourcePath, "gt-made")
+    gtUnpackedFlag = os.path.join(globusSourcePath, "gt-unpacked")
+    gtConfiguredFlag = os.path.join(globusSourcePath, "gt-configured")
+    gtMadeFlag = os.path.join(globusSourcePath, "gt-made")
+
+    # Are we clearing any previous state?
+    if clearPrevious:
+        print("Clearing previous state...")
+        for file in [gtInstalledFlag,
+                     gtUnpackedFlag,
+                     gtConfiguredFlag,
+                     gtMadeFlag]:
+            if os.path.exists(file):
+                os.remove(file)
+        os.system("rm -rf " + os.path.join(tmpDir, "*"))
+        os.system("rm -rf " + os.path.join(globusLocation, "*"))
 
     # Are we already done?
     if os.path.exists(gtInstalledFlag):
-        logMsg("Flag file %s exists. GT appears to be already installed"
-               % gtInstalledFlag)
-        sys.exit(0)
+        errorExit("Flag file %s exists. GT appears to be already installed"
+                  % gtInstalledFlag, exitValue=0)
 
+    # OK, have at it...
+
+    # Start up out output handler. Do this after clearing any previous
+    # state, which could delete the log file created by the handler.
+    output = OutputHandler(logFilePath, appendLog)
+ 
     # Figure out the URL to the Globus tarball
     (globusMajorVersion,
      globusMinorVersion,
      globusPointVersion) = globusVersion.split('.')
     # XXX Change this to http
     globusBaseURL="ftp://ftp.globus.org/pub"
-    if (globusMajorVersion = "4") and (globusMinorVersion = "0"):
+    if (globusMajorVersion == "4") and (globusMinorVersion == "0"):
         # 4.0.x URLs are of the form "gt4/4.0/4.0.4/installers/etc/..."
         globusURL = "%s/gt%s/%s.%s/%s/installers/src/%s" % (globusBaseURL,
                                                             globusMajorVersion,
@@ -129,7 +206,7 @@ def main():
                                                             globusMinorVersion,
                                                             globusVersion,
                                                             globusTarball)
-    elif (globusMajorVersion = "4") and (globusMinorVersion = "1"):
+    elif (globusMajorVersion == "4") and (globusMinorVersion == "1"):
         # 4.1.x are of the form "gt4/4.1.1/installers/etc/..."
         globusURL = "%s/gt%s/%s/installers/src/%s" % (globusBaseURL,
                                                       globusMajorVersion,
@@ -144,20 +221,21 @@ def main():
     #
 
     # Start the log
-    logMessage("GT Installation started")
-    logMessage("Location: " + globusLocation)
-    logMessage("LogFile: " + logFilename)
-    logMessage("Date: " + datetime.datetime.now().strftime("%F %T"))
-    logMessage("Version: " + options.globusVersion)
-    logMessage("Temporary Directory: " + tmpDir)
-    flaggedCmd("/tmp/flag",
-               ["ls", "-l", globusLocation],
-               "Doing ls")
+    output.message("GT Installation started")
+    output.message("Location: " + globusLocation)
+    output.message("LogFile: " + logFilePath)
+    output.message("Date: " + datetime.datetime.now().strftime("%F %T"))
+    output.message("Version: " + options.globusVersion)
+    output.message("Temporary Directory: " + tmpDir)
+    output.flaggedCmd("/tmp/flag",
+                      ["ls", "-l", globusLocation],
+                      "Doing ls")
 
-    os.chdir(globusLocation)
-    flaggedCmd(globusTarball,
-               ["wget", "--progress=dot:mega", globusURL],
-               "Getting Globus source tarball from %s" % globusURL)
+    # Download tarball
+    os.chdir(sourceCachePath)
+    output.flaggedCmd(globusTarball,
+                      ["wget", "--progress=dot:mega", globusURL],
+                      "Getting Globus source tarball from %s" % globusURL)
 
     # Sanity check
     if not os.path.exists(globusTarball):
@@ -165,36 +243,35 @@ def main():
     
     # Unpack into our temporary directory
     os.chdir(tmpDir)
-    flaggedCmd(gtUnpackedFlag,
-               ["tar", "xfz", os.path.join(globusLocation,
-                                           globusTarball)],
-               "Unpacking GT tarball...")
+    output.flaggedCmd(gtUnpackedFlag,
+                      ["tar", "xfz", os.path.join(sourceCachePath,
+                                                  globusTarball)],
+                      "Unpacking GT tarball...")
 
     # Configure globus
-    os.chdir(gtSourcePath)
-    confCmdArgs = ["configure", "--prefix=" + globusLocation]
+    os.chdir(globusSourcePath)
+    confCmdArgs = ["./configure", "--prefix=" + globusLocation]
     confCmdArgs.extend(confOptions)
 
-    flaggedCmd(gtConfiguredFlag,
-               confCmdArgs,
-               "Configuring GT...")
+    output.flaggedCmd(gtConfiguredFlag,
+                      confCmdArgs,
+                      "Configuring GT...")
 
     # And build
-    flaggedCmd(gtMadeFlag,
-               ["make"],
-               "Building GT....")
+    output.flaggedCmd(gtMadeFlag,
+                      ["make"],
+                      "Building GT....")
 
     # And install
-    flaggedCmd(gtInstalledFlag,
-               ["make", "install"],
-               "Installing GT...")
+    output.flaggedCmd(gtInstalledFlag,
+                      ["make", "install"],
+                      "Installing GT...")
 
-    # Clean up
-    toTee.flush()
-    toTee.close()
-    teePipe.wait()
+    output.message("Success.")
 
-    logMessage("Success.")
+    output.cleanup()
+
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
