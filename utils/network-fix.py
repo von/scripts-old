9feac61a90fa81928953923b89afbf35d3f1d25f
von@vwelch.com <von@vwelch.com>
2010-10-17 13:21:19 -0500

    Added continuous (-c) mode.

--- utils/network-fix.py
+++ utils/network-fix.py
@@ -26,6 +26,55 @@ Binary = {
 # Assumption this will always be the case
 AIRPORT_INTERFACE = "en1"
 
+def checkAndFixNetwork(interface="en1"):
+    """Check wireless network and fix if needed.
+
+    interface should be interface to check. 'en1' is assumed.
+
+    Returns True if network was fixed, False otherwise."""
+    fixed = False  # Return value
+    if not checkAirport():
+        print "Airport is off. Powering on..."
+        fixed = True
+        if not power_on_airport():
+            raise Exception("Failed to power on airport")
+        wait_for_default_route()
+    defaultRoute = getDefaultRoute()
+    if defaultRoute:
+        print "Default route is %s" % defaultRoute
+        if checkConnectivity(defaultRoute):
+            print "Default router is reachable."
+            return fixed
+        print "Cannot reach default router."
+    else:
+        print "Couldn't determine default route."
+
+    fixed = True
+    print "Bouncing interface"
+    subprocess.check_call(["sudo", "ifconfig", AIRPORT_INTERFACE, "down"])
+    subprocess.check_call(["sudo", "ifconfig", AIRPORT_INTERFACE, "up"])
+
+    # Airport may not come back up here.
+    if not checkAirport():
+        print "Airport is off. Powering on..."
+        if not power_on_airport():
+            raise Exception("Failed to power on airport.")
+
+    # Flush DNS cache. Not sure why, but if we tried to access a site
+    # while network was down, we get redirects to OpenDNS for a while
+    # after network comes back up.
+    flush_dns_cache()
+
+    wait_for_default_route()
+
+    print "Rechecking connectivity to default route."
+    if not checkConnectivity(getDefaultRoute()):
+        raise Exception(\
+            "Cannot reach the default router after interface bounce.")
+
+    print "Success."
+    return fixed
+
 def getDefaultRoute():
     """Return our default route as a string."""
     args = ["netstat", "-rn"]
@@ -100,51 +149,33 @@ def main(argv=None):
         usage="%prog [<options>] <some arg>", # printed with -h/--help
         version="%prog 1.0" # automatically generates --version
         )
+    parser.add_option("-c", "--continuous",
+                      action="store_true", dest="continuous",
+                      help="run continuously", default=False)
+    parser.add_option("-d", "--delay",
+                      action="store", type="int", dest="delay",
+                      help="set delay for running continuously to DELAY",
+                      metavar="DELAY", default=5)
     parser.add_option("-q", "--quiet", action="store_true", dest="quiet",
                       help="run quietly", default=False)
     (options, args) = parser.parse_args()
     if options.quiet:
         print "Running quietly..."
-    if not checkAirport():
-        print "Airport is off. Powering on..."
-        if not power_on_airport():
-            print "Failed to power on airport."
-            return(1)
-        wait_for_default_route()
-    defaultRoute = getDefaultRoute()
-    if defaultRoute:
-        print "Default route is %s" % defaultRoute
-        if checkConnectivity(defaultRoute):
-            print "Default router is reachable."
-            return(0)
-        print "Cannot reach default router."
-    else:
-        print "Couldn't determine default route."
-
-    print "Bouncing interface"
-    subprocess.check_call(["sudo", "ifconfig", AIRPORT_INTERFACE, "down"])
-    subprocess.check_call(["sudo", "ifconfig", AIRPORT_INTERFACE, "up"])
-
-    # Airport may not come back up here.
-    if not checkAirport():
-        print "Airport is off. Powering on..."
-        if not power_on_airport():
-            print "Failed to power on airport."
-            return(1)
-
-    # Flush DNS cache. Not sure why, but if we tried to access a site
-    # while network was down, we get redirects to OpenDNS for a while
-    # after network comes back up.
-    flush_dns_cache()
-
-    wait_for_default_route()
-
-    print "Rechecking connectivity to default route."
-    if not checkConnectivity(getDefaultRoute()):
-        print "Cannot reach the default router after interface bounce."
-        return(1)
-    print "Success."
-    return(0)
+    if options.continuous:
+        print "Runing continuously..."
+        totalRuns = 0
+        failures = 0
+        while True:
+            totalRuns += 1
+            if checkAndFixNetwork():
+                failures += 1
+            print "Failure rate is %4.2f%% (%d/%d)" \
+                % (failures * 100 / totalRuns, failures, totalRuns)
+            time.sleep(options.delay)
+    result = checkAndFixNetwork()
+    if not result:
+        return 1
+    return 0
 
 if __name__ == "__main__":
     sys.exit(main())
