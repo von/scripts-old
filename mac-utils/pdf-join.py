6af7f06d73e1e487d5079c5df4f3945a042d572b
Von Welch <von@vwelch.com>
2012-11-16 18:19:56 -0500

    Added pdf-join.py

---
+++ mac-utils/pdf-join.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+"""Join multiple PDF files together into a single PDF
+
+This is a wrapper around Apple's join.py script"""
+
+import argparse
+import subprocess
+import sys
+
+# Must be called with system Python
+PYTHON="/usr/bin/python"
+
+# Apple's script
+PDF_JOIN = "/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py"
+
+def main(argv=None):
+    # Do argv default this way, as doing it in the functional
+    # declaration sets it at compile time.
+    if argv is None:
+        argv = sys.argv
+
+    # Argument parsing
+    parser = argparse.ArgumentParser(
+        description=__doc__, # printed with -h/--help
+        # Don't mess with format of description
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        # To have --help print defaults with trade-off it changes
+        # formatting, use: ArgumentDefaultsHelpFormatter
+        )
+    parser.add_argument("-o", "--output", type=str, required=True,
+                        metavar="PATH", help="Output file path")
+    parser.add_argument('pdfs', metavar='paths', type=str, nargs='+',
+                        help="paths to PDFs to join")
+    args = parser.parse_args()
+
+    retcode = subprocess.call([PYTHON, PDF_JOIN, "-o", args.output] + args.pdfs,
+                              # Clear environment as PYTHONPATH will confuse
+                              # join.py
+                              env={})
+
+    return(retcode)
+
+if __name__ == "__main__":
+    sys.exit(main())
