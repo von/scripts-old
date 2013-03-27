b26b393aa32c25029c832af1bbcc836f090c2be7
Von Welch <von@vwelch.com>
2013-03-27 18:26:01 -0400

    new file:   utils/pbpaste.py

---
+++ utils/pbpaste.py
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Wrapper around pbpaste to handle running in tmux on MaxOSX
+# and using reattach-to-user-namespace.
+#
+# See:
+# https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard
+# http://robots.thoughtbot.com/post/19398560514/how-to-copy-and-paste-with-tmux-on-mac-os-x
+# http://unix.stackexchange.com/a/32451
+
+_pbpaste=/usr/bin/pbpaste  # XXX Determine dynamically
+
+if test -n "${TMUX}" -a $(uname) = "Darwin" ; then
+    exec reattach-to-user-namespace ${_pbpaste}
+else
+    exec ${_pbpaste}
+fi
